01fccc1fbe2708f0ed86e4d1d36bc1e1871a52d2
Zack Roth <zack27r@gmail.com>
2023-11-01 23:54:38 -0400

    Updated router to redirect 404s to home page

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,19 @@ import ReactDOM from "react-dom/client";
 import Home from "./pages/home.tsx";
 import { ThemeUIProvider } from "theme-ui";
 import { theme } from "./theme.ts";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Show from "./pages/show.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/show/:id",
-    element: <Show />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeUIProvider theme={theme}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/show/:id" element={<Show />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeUIProvider>
   </React.StrictMode>
 );
