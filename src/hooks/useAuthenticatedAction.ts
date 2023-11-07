2959f2318079bcbd397e67e00779f40e281d212d
Zack Roth <zack27r@gmail.com>
2023-11-07 13:45:13 -0500

    created useAuthenticatedAction hook

---
+++ src/hooks/useAuthenticatedAction.ts
@@ -0,0 +1,22 @@
+import Cookies from "js-cookie";
+import { useContext } from "react";
+import { ModalContext } from "../components/modalcontext";
+
+// If isAuthenticated, do an action, else show password modal
+const useAuthenticatedAction = (action: () => void) => {
+  const modalContext = useContext(ModalContext);
+  const isAuthenticated = Cookies.get("isAuthenticated");
+
+  const handleAction = () => {
+    if (isAuthenticated === "true") {
+      action();
+    } else {
+      modalContext?.setOnSubmit(() => action);
+      modalContext?.setOpen(true);
+    }
+  };
+
+  return handleAction;
+};
+
+export default useAuthenticatedAction;
