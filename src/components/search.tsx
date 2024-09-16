a461c55b21e671ec8c6b037d42f05f92f0eef15d
Zack Roth <zack27r@gmail.com>
2024-09-16 16:47:50 -0400

    Implemented dynamic show list fetching, added autofocus, ui tweaks

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,5 +1,6 @@
 import Select, { StylesConfig } from "react-select";
 import { theme } from "../theme";
+import { Box, Text } from "theme-ui";
 
 export type Option = {
   label: string;
@@ -9,6 +10,8 @@ export type Option = {
 interface SearchProps {
   options: Option[];
   onChange: (newValue: number) => void;
+  loading: boolean;
+  error: boolean;
 }
 
 const selectStyle: StylesConfig = {
@@ -22,6 +25,7 @@ const selectStyle: StylesConfig = {
   indicatorSeparator: (base) => ({
     ...base,
     backgroundColor: `${theme.colors?.primary}`,
+    marginLeft: 8,
   }),
   dropdownIndicator: (base) => ({
     ...base,
@@ -40,6 +44,7 @@ const selectStyle: StylesConfig = {
     }`,
     padding: 8,
     cursor: "pointer",
+    borderBottom: `.25px solid ${theme.colors?.accent}`,
   }),
   menu: (base) => ({
     ...base,
@@ -54,17 +59,58 @@ const selectStyle: StylesConfig = {
   }),
   noOptionsMessage: (base) => ({
     ...base,
-    paddingTop: 8,
-    paddingBottom: 8,
+    padding: 8,
     fontWeight: 800,
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  }),
+  loadingIndicator: (base) => ({
+    ...base,
+    color: `${theme.colors?.primary}`,
+    paddingRight: 4,
+  }),
+  loadingMessage: (base) => ({
+    ...base,
+    padding: 8,
+    fontWeight: 800,
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
   }),
 };
 
-const Search: React.FC<SearchProps> = ({ options, onChange }) => {
+const Search: React.FC<SearchProps> = ({
+  options,
+  onChange,
+  loading,
+  error,
+}) => {
+  if (error) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+          px: 24,
+          textAlign: "center",
+        }}
+      >
+        <Text variant="heading2">something went wrong.</Text>
+        <Text>
+          check your internet connection and try again. if this issue persists,
+          contact the tech team.
+        </Text>
+      </Box>
+    );
+  }
+
   return (
     <Select
       placeholder="search for a show"
-      noOptionsMessage={() => "no shows found."}
+      noOptionsMessage={(s) => `no results for ${s.inputValue}`}
       options={options}
       styles={selectStyle}
       onChange={(val: Option | unknown) => {
@@ -72,6 +118,9 @@ const Search: React.FC<SearchProps> = ({ options, onChange }) => {
         onChange(value);
       }}
       unstyled
+      isLoading={loading}
+      loadingMessage={(s) => `loading results for ${s.inputValue}`}
+      autoFocus={true}
     />
   );
 };
